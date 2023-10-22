55dc78b52e3c750ebe145474207e7a8b966a1fba
Rehman <rk03229975701@gmail.com>
2023-10-22 01:24:06 -0700

    write program which print value 1 to 5 with comma

--- src/exercises.c
+++ src/exercises.c
@@ -50,6 +50,9 @@ int main(int argc, char **argv) {
 
   //3. Print numbers 1 thru n separated by a comma except for
   //   the last one; example: 1, 2, 3, 4, 5
+    for(int i=1; i<=5; i++){
+      printf("%d,",i);
+    }
 
   //4. Print squares of numbers 1 thru n separated by a comma
   //   except for the last one; example: 1, 4, 9, 16, 25
